a1d60d758a1093f8f0edd2eb0883310905c00420
welton kleber esteves primo <75637988+weltonabravanel@users.noreply.github.com>
2023-03-17 20:55:59 -0300

    update api
    api

--- src/api.js
+++ src/api.js
@@ -1,9 +1,18 @@
 import axios from "axios";
-const request = axios.create({
-  baseURL: "https://youtube.googleapis.com/youtube/v3/",
+const BASE_URL = "https://youtube-v3-alternative.p.rapidapi.com/"
+const options = {
+  method: 'GET',
   params: {
-    key: "AIzaSyCAZkmJKKD1ByfyyvezqVWv1ohIdarhVMY",
+    maxResults:"50"
   },
-});
+  headers: {
+    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Host': 'youtube-v3-alternative.p.rapidapi.com'
+  }
+};
 
+export const api3 = async(url) =>{
+    const {data} = await axios.get(`${BASE_URL}${url}`,options)
+    return data;
+  }
 export default request;
